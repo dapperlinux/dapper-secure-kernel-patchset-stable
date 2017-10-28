55b04df0b22fda25ddae0d477bf37414982f0c90
Matthew Ruffell <msr50@uclive.ac.nz>
2017-10-28 19:33:00 +1300

    Dapper Secure Kernel Patchset Stable 4.9.59

--- init_rebase.sh
+++ init_rebase.sh
@@ -1,5 +1,5 @@
 KERNEL_MAJOR_VERSION=4.9
-KERNEL_VERSION=4.9.58
+KERNEL_VERSION=4.9.59
 
 echo "Setting up rebase directory..."
 mkdir rebase
--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,6 @@
 DATE=`date +%Y-%m-%d`
 KERNEL_MAJOR_VERSION=4.9
-KERNEL_VERSION=4.9.58
+KERNEL_VERSION=4.9.59
 
 echo "Setting up release directory..."
 mkdir release
