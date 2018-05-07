66eb06dc493679798b6c7f6ef7d76820acae7149
Matthew Ruffell <msr50@uclive.ac.nz>
2018-05-07 16:39:54 +1200

    Dapper Secure Kernel Patchset Stable 4.9.80

--- init_rebase.sh
+++ init_rebase.sh
@@ -1,5 +1,5 @@
 KERNEL_MAJOR_VERSION=4.9
-KERNEL_VERSION=4.9.79
+KERNEL_VERSION=4.9.80
 
 echo "Setting up rebase directory..."
 rm -rf rebase
--- omitted-patches/omit-patches.sh
+++ omitted-patches/omit-patches.sh
@@ -4,6 +4,7 @@
 
 # ADD NEWEST FIRST
 
+# 4.9.80 does not need any omissions
 patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.79.patch
 patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.78.patch
 patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.77.patch
