e2422cc7352bbc341a440dd9b37e55e7fb1e7ec9
Matthew Ruffell <msr50@uclive.ac.nz>
2018-05-26 21:07:45 +1200

    Dapper Secure Kernel Patchset Stable 4.9.81

--- omitted-patches/omit-patches.sh
+++ omitted-patches/omit-patches.sh
@@ -4,6 +4,7 @@
 
 # ADD NEWEST FIRST
 
+patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.81.patch
 # 4.9.80 does not need any omissions
 patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.79.patch
 patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.78.patch
