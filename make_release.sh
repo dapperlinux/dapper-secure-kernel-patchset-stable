d9384bf06bda038298bb4aab1b16384115a0784f
Matthew Ruffell <msr50@uclive.ac.nz>
2018-06-02 11:58:17 +1200

    Dapper Secure Kernel Patchset Stable 4.9.93

--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,6 @@
 DATE=`date +%Y-%m-%d`
 KERNEL_MAJOR_VERSION=4.9
-KERNEL_VERSION=4.9.92
+KERNEL_VERSION=4.9.93
 
 echo "Setting up release directory..."
 mkdir release
--- test_patch.sh
+++ test_patch.sh
@@ -1,6 +1,6 @@
 DATE=`date +%Y-%m-%d`
 KERNEL_MAJOR_VERSION=4.9
-KERNEL_VERSION=4.9.92
+KERNEL_VERSION=4.9.93
 
 echo "Removing old kernels..."
 rm -rf test
