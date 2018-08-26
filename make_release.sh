726f37fc2d2fbc947ac155ea439ed381a4faf49e
Matthew Ruffell <msr50@uclive.ac.nz>
2018-08-26 15:29:07 +1200

    Dapper Secure Kernel Patchset Stable 4.9.123

--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,6 @@
 DATE=`date +%Y-%m-%d`
 KERNEL_MAJOR_VERSION=4.9
-KERNEL_VERSION=4.9.122
+KERNEL_VERSION=4.9.123
 
 echo "Setting up release directory..."
 mkdir release
--- omitted-patches/omit-patches.sh
+++ omitted-patches/omit-patches.sh
@@ -4,6 +4,7 @@
 
 # ADD NEWEST FIRST
 
+patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.123.patch
 patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.122.patch
 patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.121.patch
 patch -F 0 -R -p1 < ../../omitted-patches/omit-4.9.120.patch
